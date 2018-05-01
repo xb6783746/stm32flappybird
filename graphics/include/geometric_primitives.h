203872623da7f75ecf9187de0efacdffc8014bb8
vlad.novikov <novikov.cs.vsu@gmail.com>
2018-05-01 17:07:30 +0300

    add common graphics interface

---
+++ graphics/include/geometric_primitives.h
@@ -0,0 +1,58 @@
+
+#ifndef FLAPPYBIRD_GRAPHICS_PRIMITIVES_H
+#define FLAPPYBIRD_GRAPHICS_PRIMITIVES_H
+
+#include <platform.h>
+
+typedef struct gl_color_s{
+
+    fb_uint8_t red;
+    fb_uint8_t green;
+    fb_uint8_t blue;
+} gl_color_t;
+
+typedef struct gl_point_s {
+
+    fb_float32_t x;
+    fb_float32_t y;
+} gl_point_t;
+
+typedef struct gl_rectangle_s {
+
+    gl_point_t point;
+
+    fb_uint16_t width;
+    fb_uint16_t height;
+
+} gl_rectangle_t;
+
+typedef struct gl_circle_s {
+
+    gl_point_t point;
+
+    fb_uint16_t radius;
+
+} gl_circle_t;
+
+typedef struct gl_filled_rectangle_s {
+
+    gl_point_t point;
+
+    fb_uint16_t width;
+    fb_uint16_t height;
+
+    gl_color_t color;
+
+} gl_filled_rectangle_t;
+
+typedef struct gl_filled_circle_s {
+
+    gl_point_t point;
+
+    fb_uint16_t radius;
+
+    gl_color_t color;
+
+} gl_filled_circle_t;
+
+#endif //FLAPPYBIRD_GRAPHICS_PRIMITIVES_H
---
+++ graphics/include/graphics.h
@@ -0,0 +1,13 @@
+
+
+#ifndef FLAPPYBIRD_GRAPHICS_H
+#define FLAPPYBIRD_GRAPHICS_H
+
+#include <geometric_primitives.h>
+
+void graphics_draw(gl_rectangle_t *obj);
+void graphics_draw(gl_filled_rectangle_t *obj);
+void graphics_draw(gl_circle_t *obj);
+void graphics_draw(gl_filled_circle_t *obj);
+
+#endif //FLAPPYBIRD_GRAPHICS_H
