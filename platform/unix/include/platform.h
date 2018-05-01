44963cd9f2f7c94d4401367c83a251d2b446e805
vlad.novikov <novikov.cs.vsu@gmail.com>
2018-05-01 16:54:42 +0300

    add typedefs for unix platform

---
+++ platform/unix/include/platform.h
@@ -0,0 +1,22 @@
+
+#ifndef FLAPPYBIRD_PLATFORM_H
+#define FLAPPYBIRD_PLATFORM_H
+
+#include <stdint.h>
+
+typedef uint8_t fb_uint8_t;
+typedef int8_t fb_int8_t;
+
+typedef uint16_t fb_uint16_t;
+typedef int16_t fb_int16_t;
+
+typedef uint32_t fb_uint32_t;
+typedef int32_t fb_int32_t;
+
+typedef uint64_t fb_uint64_t;
+typedef int64_t fb_int64_t;
+
+typedef float fb_float32_t;
+typedef double fb_float64_t;
+
+#endif //FLAPPYBIRD_PLATFORM_H
