70225a5fbc7bc9e0f3c8a68ec7c944c5271ef5ab
vlad.novikov <novikov.cs.vsu@gmail.com>
2018-05-01 16:31:26 +0300

    add toolchain files

---
+++ toolchainfiles/stm32f4.cmake
@@ -0,0 +1,22 @@
+
+ 
+SET(CMAKE_SYSTEM_NAME Generic)
+SET(CMAKE_SYSTEM_VERSION 1)
+ 
+set(PLATFORM STM32F4)
+
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+ 
+SET(LINKER_SCRIPT ${CMAKE_SOURCE_DIR}/libs/stm32/STM32F407VGTx_FLASH.ld)
+
+SET(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+SET(COMMON_FLAGS "-mcpu=cortex-m4 -mthumb -mthumb-interwork -mfloat-abi=hard -mfpu=fpv4-sp-d16 -ffunction-sections -fdata-sections -g -fno-common --specs=nano.specs --specs=nosys.specs -fmessage-length=0 --static -nostartfiles")
+
+SET(CMAKE_CXX_FLAGS_INIT "${COMMON_FLAGS} -std=c++11")
+SET(CMAKE_C_FLAGS_INIT "${COMMON_FLAGS} -std=gnu99")
+SET(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,-gc-sections -T ${LINKER_SCRIPT}")
+
+add_definitions(-DUSE_STDPERIPH_DRIVER -DSTM32F40_41xxx)
+
