8c3fc23e98844f64e0a4668cf850a47fa9c9de79
vlad.novikov <novikov.cs.vsu@gmail.com>
2018-05-01 17:16:11 +0300

    add internal headers for physics engine

---
+++ engine/include/internal/collision.h
@@ -0,0 +1,8 @@
+
+
+#ifndef FLAPPYBIRD_COLLISION_H
+#define FLAPPYBIRD_COLLISION_H
+
+void gn_phys_process_collision(void);
+
+#endif //FLAPPYBIRD_COLLISION_H
---
+++ engine/include/internal/moving.h
@@ -0,0 +1,11 @@
+
+#ifndef FLAPPYBIRD_MOVING_H
+#define FLAPPYBIRD_MOVING_H
+
+#include <platform.h>
+
+void gn_phys_move(void);
+void gn_phys_set_step(fb_float32_t step);
+void gn_phys_set_gravity(fb_float32_t g);
+
+#endif //FLAPPYBIRD_MOVING_H
