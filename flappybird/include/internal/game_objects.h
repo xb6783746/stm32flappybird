c5f2d5c11c4d696904608891f2faaeaba22d0537
vlad novikov <novikov.cs.vsu@gmail.com>
2018-05-02 15:24:23 +0300

    add game objects structs

---
+++ flappybird/include/internal/game_objects.h
@@ -0,0 +1,12 @@
+
+#ifndef FLAPPYBIRD_GAME_OBJECTS_H
+#define FLAPPYBIRD_GAME_OBJECTS_H
+
+typedef struct flappybird_bird_s{
+
+    gn_game_object *go;
+    fb_uint8_t hp;
+
+} flappybird_bird_t;
+
+#endif //FLAPPYBIRD_GAME_OBJECTS_H
