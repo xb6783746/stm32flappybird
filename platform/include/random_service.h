ab11b75462e2b19c3c8109ce742973cf4b14af2f
vlad novikov <novikov.cs.vsu@gmail.com>
2018-05-02 19:59:14 +0300

    fix random service

--- platform/include/random_service.h
+++ platform/include/random_service.h
@@ -4,6 +4,7 @@
 
 #include <platform.h>
 
-fb_uint32_t fb_random(void);
+fb_float32_t fb_random(void);
+fb_uint32_t fb_random_between(fb_uint32_t from, fb_uint32_t to);
 
 #endif //FLAPPYBIRD_RANDOM_SERVICE_H
--- platform/unix/src/random_service.c
+++ platform/unix/src/random_service.c
@@ -2,7 +2,12 @@
 #include <random_service.h>
 #include <stdlib.h>
 
-fb_uint32_t fb_random(){
+fb_float32_t fb_random(){
 
-    return (fb_uint32_t) (rand() / RAND_MAX);
+    return (fb_float32_t) (rand() / (fb_float32_t)RAND_MAX);
+}
+
+fb_uint32_t fb_random_between(fb_uint32_t from, fb_uint32_t to){
+
+    return (fb_uint32_t) (rand() % (to - from) + from);
 }
